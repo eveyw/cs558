fcf556e58337f5f543cc56a68fadada43534bdf5
Wen Yang <wyang41@binghamton.edu>
2018-02-02 18:44:19 -0500

    trial

--- a1_wyang41/PART1_Socket/cli.c
+++ a1_wyang41/PART1_Socket/cli.c
@@ -3,23 +3,23 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <strings.h>
- #include <netinet/in.h>
-     #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(int argc, char **argv) {
-        int  sockfd, n; 
+        int  sockfd, n;
         char recvline[100];
         struct sockaddr_in servaddr;
 
 	if(argc!=2){
 		printf("Usage : gettime <IP address>");
-		exit(1); 
+		exit(1);
 	}
 
         /* Create a TCP socket */
 	if((sockfd=socket(AF_INET,SOCK_STREAM, 0)) < 0){
 			perror("socket"); exit(2);}
-	    
+
         /* Specify server’s IP address and port */
         bzero(&servaddr, sizeof(servaddr));
         servaddr.sin_family = AF_INET;
@@ -30,19 +30,17 @@ int main(int argc, char **argv) {
         }
 
 	/* Connect to the server */
-       if (connect(sockfd,  (struct sockaddr *) &servaddr,sizeof(servaddr)) 
-< 0 ) {
+       if(connect(sockfd,(struct sockaddr *) &servaddr,sizeof(servaddr))<0){
                  perror("connect"); exit(4); }
 
 
         /* Read the date/time from socket */
         while ( (n = read(sockfd, recvline, 100)) > 0) {
                 recvline[n] = '\0';        /* null terminate */
-                printf("%s", recvline); 
-        }               
-        
+                printf("%s", recvline);
+        }
+
 	if (n < 0) { perror("read"); exit(5); }
 
         close(sockfd);
-} 
-
+}
